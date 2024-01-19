02c4e969c62a9589e3e4e556d023a84019daa487
Alex Schäfer <a.schaefer@app-concept.de>
2024-01-19 11:37:06 +0100

    Initial Commit

---
+++ SwiftShowcase/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftShowcase
+//
+//  Created by Alex Schäfer on 19.01.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SwiftShowcase/SwiftShowcaseApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftShowcaseApp.swift
+//  SwiftShowcase
+//
+//  Created by Alex Schäfer on 19.01.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftShowcaseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
