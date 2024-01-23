5635f528c902d49dda23d50555593eb7f07dc86a
Alex Schäfer <a.schaefer@app-concept.de>
2024-01-23 10:54:24 +0100

    feat: initial add Rockets Scene

---
+++ SwiftShowcase/Scenes/Rockets/RocketsListView.swift
@@ -0,0 +1,18 @@
+//
+//  RocketsListView.swift
+//  SwiftShowcase
+//
+//  Created by Alex Schäfer on 23.01.24.
+//
+
+import SwiftUI
+
+struct RocketsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RocketsListView()
+}
---
+++ SwiftShowcase/Scenes/Rockets/RocketsListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RocketsListViewModel.swift
+//  SwiftShowcase
+//
+//  Created by Alex Schäfer on 23.01.24.
+//
+
+import Foundation
